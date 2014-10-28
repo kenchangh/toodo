81f25eb9c2c3885b339361f20902127dabf327d6
Howard Chan <guanhao3797@gmail.com>
2014-10-28 17:54:53 +0800

    Split into toodo.js and made things clearer

--- index.js
+++ index.js
@@ -7,18 +7,26 @@ var fs = require('fs');
 var path = require('path');
 var program = require('commander');
 var utils = require('./lib/utils');
-var exports = module.exports;
 
 // Normalized paths
-var dumpPath = path.join(utils.getUserHome(), '.toodo');
-var desktopPath = path.join(utils.getUserHome(), 'Desktop');
+var DUMP_PATH = path.join(utils.getUserHome(), '.toodo');
+var DESKTOP_PATH = path.join(utils.getUserHome(), 'Desktop');
 
 /**
  * Module exports.
  */
-exports.dumpPath = dumpPath;
-exports.desktopPath = desktopPath;
+var exports = module.exports;
+exports.extend({
+  DUMP_PATH: DUMP_PATH,
+  DESKTOP_PATH: DESKTOP_PATH
+});
+
+/**
+ * Dependencies that require this module's exports,
+ * therefore placed below exports.
+ */
 var dump = require('./lib/dump');
+var toodo = require('./lib/toodo');
 
 /**
  * Self-calling function to check if dump file present.
@@ -28,7 +36,7 @@ var dump = require('./lib/dump');
  */
 (function startupCheck() {
   // Add more checks here
-  if (!fs.existsSync(dumpPath)) {
+  if (!fs.existsSync(DUMP_PATH)) {
     dump.make();
   }
 })();
@@ -47,13 +55,14 @@ program
 program
   .command('add <item>')
   .action(function (item) {
-    toodoAdd(item);
+    toodo.add(item);
   });
 
 /**
- * Finally parsing the argv.
+ * Finally parsing the argv. No extending program after this.
  */
 program.parse(process.argv);
+
 
 // MUST be behind program.parse
 var NO_COMMAND = program.args.length === 0;
@@ -64,57 +73,5 @@ var NO_COMMAND = program.args.length === 0;
  * @prints toodo list
  */
 if (NO_COMMAND) {
-  toodoRead();
-}
-
-
-function toodoRead() {
-  // Read from dump file .toodo
-  var dumpData = dump.get();
-  console.log(dumpData);
-}
-
-/**
- * Add item to list aka Desktop.
- *
- * @api private
- */
-function toodoAdd(item) {
-  // Update dump file .toodo
-  var dumpData = dump.get();
-  var itemData = {
-    name: item,
-    created: utils.getDateString()
-  };
-  dumpData.push(itemData);
-  dump.set(dumpData);
-
-  // Write to Desktop
-  var itemPath = path.join(desktopPath, item);
-  fs.writeFile(itemPath, '', function(err) {
-    if (err) {
-      console.log(err);
-    }
-    else if (program.verbose) {
-      console.log("'%s' added to toodo.", item);
-    }
-  });
-}
-
-/**
- * Remove item from list aka Desktop.
- *
- * @api private
- */
-function toodoRemove(item) {
-  // Delete file at Desktop
-  var itemPath = path.join(desktopPath, item);
-  fs.unlink(itemPath, function(err) {
-    if (err) {
-      console.log(err);
-    }
-    else if (program.verbose) {
-      console.log("'%s' removed.", item);
-    }
-  });
+  toodo.read();
 }
